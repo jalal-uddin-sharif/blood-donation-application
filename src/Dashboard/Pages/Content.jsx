1af1b5c29ac2adf123e49eaf79134977f66a7700
unknown <hycute.ts@gmail.com>
2024-06-09 18:40:11 +0600

    add filter func to all blogs in admin section

--- src/Dashboard/Pages/Content.jsx
+++ src/Dashboard/Pages/Content.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import useAxiosSecure from "../../CustomHooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import {
+  MdOutlinePublishedWithChanges,
+  MdOutlineUnpublished,
+  MdUnpublished,
+} from "react-icons/md";
 
 const Content = () => {
   const posts = [
     {
       title: "What is SaaS? Software as a Service Explained",
-      content: "Going into this journey, I had a standard therapy regimen, based on looking at the research literature. After I saw the movie, I started to ask other people what they did for their anxiety, and some",
+      content:
+        "Going into this journey, I had a standard therapy regimen, based on looking at the research literature. After I saw the movie, I started to ask other people what they did for their anxiety, and some",
 
       thumbnail:
         "https://images.unsplash.com/photo-1556155092-490a1ba16284?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
@@ -18,7 +24,8 @@ const Content = () => {
     },
     {
       title: "A Quick Guide to WordPress Hosting",
-      content: "According to him, â€œI'm still surprised that this has happened. But we are surprised because we are so surprised.â€More revelations about Whittington will be featured in the film",
+      content:
+        "According to him, â€œI'm still surprised that this has happened. But we are surprised because we are so surprised.â€More revelations about Whittington will be featured in the film",
 
       thumbnail:
         "https://images.unsplash.com/photo-1620287341056-49a2f1ab2fdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
@@ -29,7 +36,8 @@ const Content = () => {
     },
     {
       title: "7 Promising VS Code Extensions Introduced in 2022",
-      content: "I hope I remembered all the stuff that they needed to know. They're like, 'okay,' and write it in their little reading notebooks. I realized today that I have all this stuff that",
+      content:
+        "I hope I remembered all the stuff that they needed to know. They're like, 'okay,' and write it in their little reading notebooks. I realized today that I have all this stuff that",
 
       thumbnail:
         "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
@@ -40,7 +48,8 @@ const Content = () => {
     },
     {
       title: "How to Use Root C++ Interpreter Shell to Write C++ Programs",
-      content: "The powerful gravity waves resulting from the impact of the planets' moons â€” four in total â€” were finally resolved in 2015 when gravitational microlensing was used to observe the",
+      content:
+        "The powerful gravity waves resulting from the impact of the planets' moons â€” four in total â€” were finally resolved in 2015 when gravitational microlensing was used to observe the",
 
       thumbnail:
         "https://images.unsplash.com/photo-1617529497471-9218633199c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
@@ -51,15 +60,15 @@ const Content = () => {
     },
   ];
 
-  const myAxios = useAxiosSecure()
-  const {data: blogs} = useQuery({
-    queryKey:["all-blogs"],
+  const [blogStatus, setBlogStatus] = useState("")
+  const myAxios = useAxiosSecure();
+  const { data: blogs } = useQuery({
+    queryKey: ["all-blogs"],
     queryFn: async () => {
-        const blogs = await myAxios("/all-blogs")
-        return blogs.data
-    }
-  })
-  console.log(blogs);
+      const blogs = await myAxios(`/all-blogs?status=${blogStatus}`);
+      return blogs.data;
+    },
+  });
 
   //
   const getPlainText = (htmlContent) => {
@@ -67,6 +76,11 @@ const Content = () => {
     const doc = parser.parseFromString(htmlContent, "text/html");
     return doc.body.textContent || "";
   };
+
+  const handleStatus = async(id, status) => {
+    const {data} = await myAxios.patch(`/update-blog-status/${id}?status=${status}`)
+  };
+
   return (
     <div>
       <div className="w-full flex justify-end">
@@ -87,7 +101,9 @@ const Content = () => {
         <div className="mt-12 grid gap-5 sm:grid-cols-2 lg:grid-cols-4 ">
           {blogs?.map((items, key) => (
             <article
-              className={`max-w-md mx-auto mt-4 shadow-lg border  p-2 rounded-md duration-300 hover:shadow-sm ${items.status === "draft" ? "bg-red-300" : "bg-gray-50"}`} 
+              className={`max-w-md mx-auto mt-4 shadow-lg border  p-2 rounded-md duration-300 hover:shadow-sm ${
+                items.status === "draft" ? "bg-red-300" : "bg-gray-100"
+              }`}
               key={key}
             >
               <a href={items.href}>
@@ -120,8 +136,25 @@ const Content = () => {
                     {getPlainText(items.content).substring(0, 200)}...
                   </p>
                 </div>
-                <div>
-                    <h1>Blog Status: {items.status} </h1>
+                <div className="px-4 flex items-center  justify-between">
+                  <h1>Blog Status: {items.status} </h1>
+                  {items.status === "draft" ? (
+                    <button
+                      onClick={() => handleStatus(items._id, "published")}
+                      title="click for published"
+                      className="btn btn-circle"
+                    >
+                      <MdOutlinePublishedWithChanges color="green" size={25} />
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => handleStatus(items._id, "draft")}
+                      title="click for unpublished"
+                      className="btn btn-circle bg-white"
+                    >
+                      <MdOutlineUnpublished color="red" size={25} />
+                    </button>
+                  )}
                 </div>
               </a>
             </article>
